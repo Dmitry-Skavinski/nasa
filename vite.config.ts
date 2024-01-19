348f982aa03372b80754d2f12fe8297b52a51301
Dmitry Skavinski <d.skavinskiy@mail.ru>
2024-01-19 12:07:04 +0300

    some fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-        scss: {
-            additionalData: '@import "./src/common/scss/index.scss";',
+    base: '/nasa/',
+    plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@import "./src/common/scss/index.scss";',
+            },
         },
     },
-},
 })
